d612965968eacd25a75148769522fdcb1b270ae1
Yusuke060 <48151447+Yusuke060@users.noreply.github.com>
2020-04-13 17:26:51 +0900

    Add files via upload

--- web/callback.php
+++ web/callback.php
@@ -24,116 +24,116 @@ if($obj != "text"){
 
 //返信データ作成
 if ($text == 'はい') {
-  $response_format_text = [
+  $response_format_text = array(
     "type" => "template",
     "altText" => "こちらの〇〇はいかがですか？",
-    "template" => [
+    "template" => array(
       "type" => "buttons",
       "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img1.jpg",
       "title" => "○○レストラン",
       "text" => "お探しのレストランはこれですね",
-      "actions" => [
-          [
+      "actions" => array(
+          array(
             "type" => "postback",
             "label" => "予約する",
             "data" => "action=buy&itemid=123"
-          ],
-          [
+          ),
+          array(
             "type" => "postback",
             "label" => "電話する",
             "data" => "action=pcall&itemid=123"
-          ],
-          [
+          ),
+          array(
             "type" => "uri",
             "label" => "詳しく見る",
             "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
-          ],
-          [
+          ),
+          array(
             "type" => "message",
             "label" => "違うやつ",
             "text" => "違うやつお願い"
-          ]
-      ]
-    ]
-  ];
+          )
+      )
+    )
+  );
 } else if ($text == 'いいえ') {
   exit;
 } else if ($text == '違うやつお願い') {
-  $response_format_text = [
+  $response_format_text = array(
     "type" => "template",
     "altText" => "候補を３つご案内しています。",
-    "template" => [
+    "template" => array(
       "type" => "carousel",
-      "columns" => [
-          [
+      "columns" => array(
+          array(
             "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img2-1.jpg",
             "title" => "●●レストラン",
             "text" => "こちらにしますか？",
-            "actions" => [
-              [
+            "actions" => array(
+              array(
                   "type" => "postback",
                   "label" => "予約する",
                   "data" => "action=rsv&itemid=111"
-              ],
-              [
+              ),
+              array(
                   "type" => "postback",
                   "label" => "電話する",
                   "data" => "action=pcall&itemid=111"
-              ],
-              [
+              ),
+              array(
                   "type" => "uri",
                   "label" => "詳しく見る（ブラウザ起動）",
                   "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
-              ]
-            ]
-          ],
-          [
+              )
+            )
+          ),
+          array(
             "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img2-2.jpg",
             "title" => "▲▲レストラン",
             "text" => "それともこちら？（２つ目）",
-            "actions" => [
-              [
+            "actions" => array(
+              array(
                   "type" => "postback",
                   "label" => "予約する",
                   "data" => "action=rsv&itemid=222"
-              ],
-              [
+              ),
+              array(
                   "type" => "postback",
                   "label" => "電話する",
                   "data" => "action=pcall&itemid=222"
-              ],
-              [
+              ),
+              array(
                   "type" => "uri",
                   "label" => "詳しく見る（ブラウザ起動）",
                   "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
-              ]
-            ]
-          ],
-          [
+              )
+            )
+          ),
+          array(
             "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img2-3.jpg",
             "title" => "■■レストラン",
             "text" => "はたまたこちら？（３つ目）",
-            "actions" => [
-              [
+            "actions" => array(
+              array(
                   "type" => "postback",
                   "label" => "予約する",
                   "data" => "action=rsv&itemid=333"
-              ],
-              [
+              ),
+              array(
                   "type" => "postback",
                   "label" => "電話する",
                   "data" => "action=pcall&itemid=333"
-              ],
-              [
+              ),
+              array(
                   "type" => "uri",
                   "label" => "詳しく見る（ブラウザ起動）",
                   "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
-              ]
-            ]
-          ]
-      ]
-    ]
-  ];
+              )
+            )
+          )
+      )
+    )
+  );
 } else if ($text == 'おはよう') {
     $response_format_text = array(
         'type' => 'text',
@@ -153,8 +153,8 @@ if ($text == 'はい') {
 } else if ($text == '写真') {
     $response_format_text = array(
         'type'      => 'image',
-        'packageId' => '/img2-3.jpg',
-        'stickerId' => '/img2-3.jpg'
+        'packageId' => "https://" . $_SERVER['SERVER_NAME'] . "/img2-3.jpg",
+        'stickerId' => "https://" . $_SERVER['SERVER_NAME'] . "/img2-3.jpg"
     );
 } else if ($text == 'sticker') {
     $response_format_text = array(
@@ -163,32 +163,32 @@ if ($text == 'はい') {
         'stickerId' => 1
     );
 } else {
-  $response_format_text = [
+  $response_format_text = array(
     "type" => "template",
     "altText" => "こんにちわ 何かご用ですか？（はい／いいえ）",
-    "template" => [
+    "template" => array(
         "type" => "confirm",
         "text" => "こんにちわ 何かご用ですか？",
-        "actions" => [
-            [
+        "actions" => array(
+            array(
               "type" => "message",
               "label" => "はい",
               "text" => "はい"
-            ],
-            [
+            ),
+            array(
               "type" => "message",
               "label" => "いいえ",
               "text" => "いいえ"
-            ]
-        ]
-    ]
-  ];
+            )
+        )
+    )
+  );
 }
 
-$post_data = [
+$post_data = array(
 	"replyToken" => $replyToken,
-	"messages" => [$response_format_text]
-	];
+	"messages" => array($response_format_text)
+);
 
 $ch = curl_init("https://api.line.me/v2/bot/message/reply");
 curl_setopt($ch, CURLOPT_POST, true);

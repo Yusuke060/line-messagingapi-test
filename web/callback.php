be4a03a43f61951c9ca9615ba41d919a8150bf21
Yusuke060 <48151447+Yusuke060@users.noreply.github.com>
2020-04-22 22:50:17 +0900

    Add files via upload

--- web/callback.php
+++ web/callback.php
@@ -425,14 +425,14 @@ if($type == 'message') {
             $response_format_text = array(
                 array(
                     'type' => 'text',
-                    'text' => '【'.$menu[2][1].'】'
+                    'text' => '【'.$menu[2].'】'
                 )
             );
         } else {
             $response_format_text = array(
                 array(
                     'type' => 'text',
-                    'text' => '【'.$menu[1][1].'】'
+                    'text' => '【'.$menu[1].'】'
                 )
             );
         }

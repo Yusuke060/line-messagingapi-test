50c561ccfccac2ae83bc6c931c2509a4c1aafb4e
Yusuke060 <48151447+Yusuke060@users.noreply.github.com>
2020-04-25 20:39:56 +0900

    Add files via upload

--- web/callback.php
+++ web/callback.php
@@ -167,7 +167,7 @@ if($type == 'message') {
         } else if ($text == '天気') {
             
             $weather = json_decode(file_get_contents('https://api.openweathermap.org/data/2.5/forecast?id=1907306&units=metric&appid=87ad375ecb53eaf114f39defb035f7c9'), true);
-            $tenki = $weather->{"list"}[0]->{"weather"}[0]->{"main"};
+            $tenki = $weather->{"list"}->{"weather"}[0]->{"main"};
         
             $response_format_text = array(
                 array(

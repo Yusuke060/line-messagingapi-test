fda99d145f8bbb8fa545c619a6fb1640dce0343f
Yusuke060 <48151447+Yusuke060@users.noreply.github.com>
2020-04-28 11:44:04 +0900

    Add files via upload

--- web/callback.php
+++ web/callback.php
@@ -63,17 +63,6 @@ if($type == 'message') {
     }
 
     if($msg_obj == "text"){
-        
-        $data = fopen('https://' . $_SERVER['SERVER_NAME'] . '/database.csv', 'w');
-        while (($dcsv= fgetcsv($data)) !== false) {
-            $database[] = $dcsv;
-        }
-        $database[] = [$text, $userId];
-        foreach($database as $line){
-            fputcsv($data, $line);
-        }
-        fclose($data);
-        
         if ($text == '昼ごはん') {
             $response_format_text = array(
                 array(
